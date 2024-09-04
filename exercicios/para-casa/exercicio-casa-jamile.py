a82cce7877a547b83903b88e93b0c513f9b5ab42
Jamile Barroso <jamilebarroso0@gmail.com>
2024-09-04 06:05:10 -0300

    atv analise de dados

---
+++ exercicios/para-casa/exercicio-casa-jamile.py
@@ -0,0 +1,20 @@
+# %%
+# Exercício de Casa 
+
+# Parte 1: Netflix
+
+# %%
+# 1 - Quantos usuários utilizam a Netflix pelo Tablet?
+
+# %%
+# 2 - Qual é a representatividade (em %) do sistema operacional(device) mais utilizado para assistir Netflix?
+
+# %%
+# 3 - Crie visualizações que responda as seguintes perguntas:
+# 3.1 - Qual é o sistema operacional(device) mais usado para cada faixa etária? Existe algum padrão? O tipo de aparelho muda de acordo com o grupo de idade?
+
+# %%
+# 3 - Crie visualizações que responda as seguintes perguntas:
+# 3.2 - No Brasil, qual é o sistema operacional mais utilizado?
+
+
